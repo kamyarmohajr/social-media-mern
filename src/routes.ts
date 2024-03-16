3fa02e2809bd8a2c03a04dae92ccc503c376b484
Kamyar Mohajer <mohajer.kam20i@gmail.com>
2024-03-16 13:41:36 +0330

    added user login and registration routes

---
+++ src/routes.ts
@@ -0,0 +1,23 @@
+import { Express } from "express";
+import { createUserHandler } from "./controllers/user.controller";
+import validate from "./middlewares/validateResource";
+import { createUserSchema } from "./schema/user.schema";
+import {
+  createUserSessionHandler,
+  deleteUserSessionHandler,
+  getUserSessionsHandler,
+} from "./controllers/session.controller";
+import { createSessionSchema } from "./schema/session.schema";
+import deserializedUser from "./middlewares/deserializedUser";
+const routes = (app: Express) => {
+  app.post("/api/users", validate(createUserSchema), createUserHandler);
+  app.post(
+    "/api/session",
+    validate(createSessionSchema),
+    createUserSessionHandler
+  );
+  app.get("/api/sessions", deserializedUser, getUserSessionsHandler);
+  app.delete("/api/session", deserializedUser, deleteUserSessionHandler);
+};
+
+export default routes;
