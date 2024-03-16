464a9ace01cc6f733d1ccc9beb0c4eeda2ed1830
Kamyar Mohajer <mohajer.kam20i@gmail.com>
2024-03-16 13:32:38 +0330

    added user creation handler

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import logger from "../utils/logger";
+import { createUser } from "../service/user.service";
+import { CreateUserInput } from "../schema/user.schema";
+
+export const createUserHandler = async (
+  req: Request<{}, {}, CreateUserInput["body"]>,
+  res: Response
+) => {
+  try {
+    logger.info(req.body);
+    const user = await createUser(req.body);
+    return res.status(201).json({ user });
+  } catch (error: any) {
+    logger.error(error.message);
+    res.status(409).send(error.message);
+  }
+};
