62f85140a0994e5b70cf4dac2a3f4534490d37fc
Kamyar Mohajer <mohajer.kam20i@gmail.com>
2024-03-16 13:46:06 +0330

    added crud functions for user session

---
+++ src/service/session.service.ts
@@ -0,0 +1,57 @@
+import { FilterQuery, UpdateQuery } from "mongoose";
+import SessionModel, { SessionDocument } from "../models/session.model";
+import { omit } from "lodash";
+import { signJwt, verifyJwt } from "../utils/jwt.utils";
+import { UserDocument } from "../models/user.model";
+import { findUser } from "./user.service";
+import { get } from "lodash";
+import config from "config";
+
+export const createUserSession = async (userId: string, userAgent: string) => {
+  try {
+    const session = await SessionModel.create({ user: userId, userAgent });
+    return omit(session.toJSON(), "password");
+  } catch (error: any) {
+    return new Error(error.message);
+  }
+};
+
+export const findUserSessions = async (query: FilterQuery<SessionDocument>) => {
+  try {
+    return await SessionModel.find(query).lean();
+  } catch (error: any) {
+    return new Error(error.message);
+  }
+};
+
+export const updateUserSession = async (
+  query: FilterQuery<SessionDocument>,
+  update: UpdateQuery<SessionDocument>
+) => {
+  return await SessionModel.updateOne(query, update).lean();
+};
+
+export const reIssueAccessToken = async ({
+  refreshToken,
+}: {
+  refreshToken: string;
+}) => {
+  const { decoded } = await verifyJwt(refreshToken);
+
+  const session = await SessionModel.findById(get(decoded, "session"));
+
+  if (!session || !session.valid) return new Error("not valid");
+
+  const user = await findUser({ _id: session.user });
+
+  if (!user) return new Error("user not found");
+
+  const accessToken = signJwt(
+    { ...user, session: session._id },
+    {
+      expiresIn: config.get<string>("accessTokenTtl"),
+    }
+  );
+
+  return accessToken;
+};
